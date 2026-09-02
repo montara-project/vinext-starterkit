82c7527cdf063c0663f3b2aa23466a9a5125b9a2
masb0ymas <n.fajri@outlook.com>
2026-09-02 17:52:10 +0700

    fix: problem next-intl

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vinext from 'vinext'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['next-intl'],
+  },
   plugins: [
     vinext({
       cache: { cdn: cdnAdapter() },
